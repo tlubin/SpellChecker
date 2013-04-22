30c9cc32bb6be1cf56945c20755b074923e250b1
Todd Lubin <todd.lubin@mail.hsa.net>
2013-04-21 20:24:26 -0400

    commit for peter

--- dict.ml
+++ dict.ml
@@ -6,8 +6,19 @@ sig
   val create : string -> dict_t
 
   (** return the next valid entry in the dictionary at or past the input *)
-  val next_entry: string -> string
+  val next_entry: dict_t -> string -> string
 end
 
 module Dict : DICT =
-  failwith "implement me"
+struct
+  type dict_t = string array
+
+  let create filename =
+    (* read in file from /usr/share/dict/web2 into an array *)
+    failwith "implement"
+
+  let next_entry dict word =
+    (* recursively find the next word, inclusive *)
+    failwith "implement"
+
+end
