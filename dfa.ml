90a8e57fa85852409af4f75ef19fe25203ba0ab3
Todd Lubin <todd.lubin@mail.hsa.net>
2013-04-21 23:57:10 -0400

    dfa functions almost done

--- dfa.ml
+++ dfa.ml
@@ -4,11 +4,9 @@ module type DFA =
 sig
   type dfa_t
   
-  (** return the next valid string satisfying the dfa *)
+  (* return the next valid string satisfying the dfa *)
   val next_valid_string: string -> string
 
-    (* stuff needed to build the dfa. i.e. add_transition,... *)
-  
   (* singleton returns a dfa, with given start state *)
   val singleton: Type.state -> dfa_t
 
@@ -31,10 +29,32 @@ struct
       let compare a b = compare a b
     end)
 
+  (* get the next valid lexigraphical string in the DFA *)
+  let next_valid_string current =
+    failwith "todo"
+
   (* return a singleton dfa with the given state *)
   let singleton (state: Type.state) = Type.StateDict.singleton state (TranDict.empty)
 
+  (* add a transition from orig to dest using transition type trans *)
   let add_transition my_dfa orig trans dest =
+    let trans_dict, start, final_states = my_dfa in
+    if Type.StateSet.mem orig trans_dict then 
+      (let inner_dict = Type.StateSet.find orig trans_dict in
+       assert not (TranDict.mem trans inner_dict);
+       let inner_dict' = TranDict.add trans dest inner_dict in
+       let trans_dict' = Type.StateSet.add orig inner_dict' trans_dict in
+      (trans_dict', start, final_states))
+    else
+      let inner_dict = TranDict.singleton trans dest in
+      let trans_dict' = Type.StateSet.add orig inner_dict trans_dict in
+      (trans_dict', start, final_states)
+
+  (* add a state to be a final state *)
+  let add_final my_dfa final =
+    let trans_dict, start, final_states = my_dfa in
+    let final_states' = Type.StateSet.add final final_states in
+    (trans_dict, start, final_states')
     
 
 end
