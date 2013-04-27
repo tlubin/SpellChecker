ec4d9009f42e7dffc97cbb4165d7e0b5005458e7
Todd Lubin <todd.lubin@mail.hsa.net>
2013-04-27 19:47:25 -0400

    working with command line arguments

--- main.ml
+++ main.ml
@@ -3,9 +3,20 @@ open Nfa
 open Dict
 open Lev
 
-(* this is going to be the main file for the command line thing *)
-module MyLev = Levenshtein (Nfa) (Dfa) (Dict);;
+module MyLev = Levenshtein (Nfa) (Dfa) (Dict)
 
-let dict = Sys.argv.(1);;
-let matches = MyLev.find_matches "nice" 1 dict;;
-List.iter print_string matches;;
+let usage = "Usage: ./spellchecker dictpath dictcount word edit_d\n" ^
+    "wc -l dictpath to get the dictcount\n"
+
+let main () =
+  if Array.length Sys.argv <> 5 then 
+    (print_string usage; exit 0)
+  else
+  let dict = Sys.argv.(1) in
+  let word = Sys.argv.(3) in
+  let edit_d = int_of_string Sys.argv.(4) in
+  let matches = MyLev.find_matches word edit_d dict in
+  List.iter (fun x -> print_string (x ^ "\n")) matches
+;;
+
+main();;
