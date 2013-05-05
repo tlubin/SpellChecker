9d8009e6f5fa2a1c39e9272397018d7adfc3f14f
Wilson Qin <wilson.qin@gmail.com>
2013-05-05 15:27:38 -0400

    now with quit action, and reindentations

--- main.ml
+++ main.ml
@@ -8,11 +8,16 @@ module MyLev = Levenshtein (Nfa) (Dfa) (Dict)
 
 let max_edit = 5;;
 
-type mode = Word | Sentence | File;;
+type mode = Word | Sentence | File ;;
+
+(** 
+  returns true if it is a string that should
+  be skipped and does not need a match *)
+let do_skip str = Str.string_match (Str.regexp ".*-.*") str 0
 
 (** prepare a string by stripping bad characters, & lowercasing *)
 let prepare str = 
-  let stripped = Str.global_replace (Str.regexp "[^a-zA-Z ]") "" str in
+  let stripped = Str.global_replace (Str.regexp "[^a-zA-Z- ]") "" str in
   let stripped = Str.global_replace (Str.regexp "[ ]+") " " stripped in
   String.lowercase stripped
 
@@ -32,17 +37,21 @@ let get_line () =
 let get_file () = 
   print_string "Enter absolute or relative path for file: ";
   let filepath = String.trim (read_line()) in
-  let input_ch = open_in filepath in
-  let lines = ref [] in
   try
-    while true do
-      let clean_line = prepare (String.trim (input_line input_ch)) in
-      lines := clean_line :: !lines
-    done;
-    !lines
-  with End_of_file ->
-    close_in input_ch;
-    List.rev !lines
+    let input_ch = open_in filepath in
+    let lines = ref [] in
+    try
+      while true do
+        let clean_line = prepare (String.trim (input_line input_ch)) in
+        lines := clean_line :: !lines
+      done;
+      !lines
+    with End_of_file ->
+      close_in input_ch;
+      List.rev !lines
+  with Sys_error(msg) ->
+    Printf.printf "%s\n" msg;
+    []
 
 
 
@@ -54,12 +63,13 @@ let get_editd () =
 (** Get the current operation mode. *)
 let get_mode () =
   let rec get_mode_helper () : mode = 
-    print_string "Would you like to enter word mode (W), sentence mode (S), or file mode (F)? : ";
+    print_string "Would you like to enter word mode (W), sentence mode (S), file mode (F), or quit (Q)? : ";
     let x = String.trim (String.lowercase (read_line ())) in
     match x with
     | "w" -> Word 
     | "s" -> Sentence
     | "f" -> File
+    | "q" -> exit 0 (* quit the program *)
     | _ -> (
       print_string "That is not a valid choice. Please try again. \n";
       get_mode_helper ()
@@ -86,47 +96,56 @@ let main () =
     (* Print Welcome *)
     Menu.print_header ();
     (* ask what mode to enter *)
-    let mode = get_mode() in
+    let mode = ref (get_mode()) in
     let do_action() =
-      match mode with
-      | Word -> 
-    	  (let word = get_word() in
-           if word = "" then Printf.printf "%s\n" "Try again. Please input alphabetic characters."
-    	   else 
-             match MyLev.find_matches word dictionary with
-      	       | [] -> Printf.printf "That is not close to a word!\n"
-      	       | ms ->
-      		 List.iter (fun (m, (r, p, s)) -> Printf.printf "%s %d %f %f\n" m r p s) ms)
-      | Sentence ->
-    	(let line = get_line() in
-    	 let words = Str.split (Str.regexp " ") line in
-    	 let corrected = List.map 
-    	   (fun w -> match MyLev.find_matches w dictionary with
-    	     | [] -> w
-    	     | (w,_)::_ -> w) words in
-    	 List.iter (fun w -> Printf.printf "%s " w) corrected;
-    	 Printf.printf "\n")
-      | File ->
-        (let line_arr = get_file() in
-          List.iteri (fun line_num line ->
-            let words = Str.split (Str.regexp " ") line in
-            List.iteri (fun word_num wd -> 
-              match MyLev.find_matches wd dictionary with
-              | [] -> 
-                  Printf.printf "(%d,%d): %s\t" line_num word_num wd;
-                  Printf.printf "%s\n" "<No better suggestions available>"
-              | lst -> 
-                let lst = cut_down 3 lst in
-		(* word is spelled correctly *)
-                if List.length lst = 1 && List.hd lst = wd then ()
-                else (
-		  (* print correction and location in file *)
-                  Printf.printf "(%d,%d): %s\t" line_num word_num wd;
-                  Printf.printf "%s\n" (String.concat ", " lst)
-                )
-            ) words
-          ) line_arr
-        )
+      (match !mode with
+        | Word -> 
+            (let word = get_word() in
+               if word = "" then Printf.printf "%s\n" "Try again. Please input alphabetic characters."
+               else if do_skip word then Printf.printf "%s\n" word
+               else 
+                 match MyLev.find_matches word dictionary with
+                   | [] -> Printf.printf "That is not close to a word!\n"
+                   | ms ->
+               List.iter (fun (m, (r, p, s)) -> Printf.printf "%s %d %f %f\n" m r p s) ms)
+        | Sentence ->
+          (let line = get_line() in
+           let words = Str.split (Str.regexp " ") line in
+           let corrected = List.map 
+             (fun w -> 
+               if do_skip w then w
+               else
+                 match MyLev.find_matches w dictionary with
+                 | [] -> w
+                 | (w,_)::_ -> w
+             ) words in
+           List.iter (fun w -> Printf.printf "%s " w) corrected;
+           Printf.printf "\n")
+        | File ->
+            (let line_arr = get_file() in
+              List.iteri (fun line_num line ->
+                let words = Str.split (Str.regexp " ") line in
+                List.iteri (fun word_num wd -> 
+                  if do_skip wd then () (* ignore strings we should skip *)
+                  else
+                    match MyLev.find_matches wd dictionary with
+                    | [] -> 
+                        Printf.printf "(%d,%d): %s\t" (line_num+1) (word_num+1) wd;
+                        Printf.printf "%s\n" "<No better suggestions available>"
+                    | lst -> 
+                      let lst = cut_down 3 lst in
+                      (* word is spelled correctly *)
+                      if List.length lst = 1 && List.hd lst = wd then ()
+                      else (
+                        (* print correction and location in file *)
+                        Printf.printf "(%d,%d): %s\t" (line_num+1) (word_num+1) wd;
+                        Printf.printf "%s\n" (String.concat ", " lst)
+                      )
+                ) words
+              ) line_arr
+            )
+      );
+      mode := get_mode ()
     in
     while true do
        do_action()
